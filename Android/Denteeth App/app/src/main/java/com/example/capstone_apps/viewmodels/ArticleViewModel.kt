4c904fb6ae3371c2ae6797bc9ae0acb05aa69440
DimasHamzah <hamzahdimas85@gmail.com>
2022-06-11 22:38:19 +0800

    create function getDetailArticle in articleViewModel

--- Android/Denteeth App/app/src/main/java/com/example/capstone_apps/viewmodels/ArticleViewModel.kt
+++ Android/Denteeth App/app/src/main/java/com/example/capstone_apps/viewmodels/ArticleViewModel.kt
@@ -3,6 +3,7 @@ package com.example.capstone_apps.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.capstone_apps.data.remote.response.ResponseArticle
+import com.example.capstone_apps.data.remote.response.ResponseDetailArticle
 import com.example.capstone_apps.helper.ResultResponse
 import com.example.capstone_apps.repository.ArticleRepository
 
@@ -10,5 +11,8 @@ class ArticleViewModel (private val articleRepository: ArticleRepository) : View
 
   fun getAllListArticle(): LiveData<ResultResponse<ResponseArticle>> =
     articleRepository.getAllArticle()
+
+  fun getDetailArticle(id: Int): LiveData<ResultResponse<ResponseDetailArticle>> =
+    articleRepository.requestDetailArticle(id)
 
 }
