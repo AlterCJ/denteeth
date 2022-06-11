14a41b78dba8d716084fc580b023a84fd5eab8f0
DimasHamzah <hamzahdimas85@gmail.com>
2022-06-11 23:06:17 +0800

    add DetailArticleActivity

---
+++ Android/Denteeth App/app/src/main/java/com/example/capstone_apps/views/detail/DetailArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstone_apps.views.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstone_apps.R
+
+class DetailArticleActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_detail_article)
+  }
+}
