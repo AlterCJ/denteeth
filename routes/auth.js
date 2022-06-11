65e411b39e2bd28bfdfb25132f3625e2008035c5
Your Name <you@egmail.com>
2022-06-11 09:56:59 +0700

    delete

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signIn, signUp } = require('../controllers/AuthController');
-
-router.post('/auth/signin',signIn);
-router.post('/auth/signup',signUp);
-
-module.exports = router;
