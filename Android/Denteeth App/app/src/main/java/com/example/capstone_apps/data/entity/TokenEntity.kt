e372f2c62a1124dae72cd4c0eda6a1fa274f5aa3
DimasHamzah <hamzahdimas85@gmail.com>
2022-06-11 22:28:52 +0800

    create class tokenentity

---
+++ Android/Denteeth App/app/src/main/java/com/example/capstone_apps/data/entity/TokenEntity.kt
@@ -0,0 +1,9 @@
+package com.example.capstone_apps.data.entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class TokenEntity(
+  var token: String? = null
+): Parcelable
