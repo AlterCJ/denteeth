46f371030f5ba9ef1837dc003fc960b85d075491
mhdfariz18_ <67161347+mhdfariz18@users.noreply.github.com>
2022-06-11 10:35:30 +0700

    Delete Backend API directory

--- Backend API/Android/Denteeth App/app/src/main/java/com/example/capstone_apps/helper/ResultResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.capstone_apps.helper
-
-sealed class ResultResponse<out R> private constructor(){
-  data class Success<out T>(val data: T) : ResultResponse<T>()
-  data class Error(val error: String): ResultResponse<Nothing>()
-  object Loading: ResultResponse<Nothing>()
-}
-
